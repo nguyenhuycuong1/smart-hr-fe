ec0b8d90140bdb69b2905d1cac3102276edd0550
nguyenhuycuong1 <nhclnhc@gmail.com>
2025-06-11 21:09:23 +0700

    reset

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,10 +1,11 @@
 export const environment = {
-    production: false,
-    api_endpoint: 'http://localhost:8080/smart-hr',
-    keycloak: {
-      issuer: 'http://localhost:8081',
-      realm: 'smart_hr',
-      clientId: 'smart_hr',
-    },
-  };
-  
+  production: false,
+  // api_endpoint: 'http://localhost:8080/smart-hr',
+  api_endpoint: 'http://13.229.103.20/smart-hr',
+  keycloak: {
+    // issuer: 'http://localhost:8081',
+    issuer: 'https://smarthr.duckdns.org',
+    realm: 'smart_hr',
+    clientId: 'smart_hr',
+  },
+};
